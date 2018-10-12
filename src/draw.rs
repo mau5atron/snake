e597489f133a25bc200b74a7632a8e85c8b7aeab
mau5atron <gabrielbetancourt96@outlook.com>
2018-10-12 00:41:42 -0400

    draw_rec helper fn

--- src/draw.rs
+++ src/draw.rs
@@ -25,3 +25,25 @@ pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d){
 		g,
 	);
 }
+
+pub fn draw_rectangle(
+	color: Color,
+	x: i32,
+	y: i32,
+	width: i32,
+	height: i32,
+	con: &Context, 
+	g: &mut G2d
+){
+	let x = to_coord(x);
+	let y = to_coord(y);
+
+	// able to control size of rectangle
+	// block_size * control size of board
+	rectangle(
+		color, 
+		[x, y, BLOCK_SIZE * (width as f64), BLOCK_SIZE * (height as f64)],
+		con.transform,
+		g,
+	);
+}
