a65180cbf75f014b0316de790176934dc7070f05
mau5atron <gabrielbetancourt96@outlook.com>
2018-10-12 00:26:54 -0400

    imported draw mod to main / using external dep

---
+++ src/draw.rs
@@ -0,0 +1,4 @@
+// import piston_window dep // definition of rectangle, 
+// Context allows drawing of 2d content, G2d to draw
+use piston_window::{rectangle, Context, G2d};
+use piston_window::types::color;
